8a6f3381b38d0d2b366010c4d82696608c0a8e11
Leo_Amv <leonidamvrosov@yandex.ru>
2022-08-04 00:47:22 +0500

    Test version of the solution added

--- Program.cs
+++ Program.cs
@@ -1,1 +1,34 @@
-﻿
+﻿using System;
+namespace FinalTestWork
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Введите несколько значений через пробел");
+            string? userInput = Console.ReadLine();
+            if (!string.IsNullOrEmpty(userInput))
+            {
+                string[] strings = userInput.Split(' ');
+                string temp = string.Empty;
+                for (int i = 0; i < strings.Length; i++)
+                {
+                    if (strings[i].Length <= 3 && strings[i] != " " && !string.IsNullOrEmpty(strings[i]))
+                    {
+                        if (i == strings.Length - 1) temp += strings[i];
+                        else temp += strings[i] + " ";
+                    }
+                }
+                string[] result = temp.Split(' ');
+                Console.WriteLine(string.Join(" ", result));
+                //Console.WriteLine($"\n{strings.Length}\t{result.Length}\t _{strings[2]}_\t_{result[2]}_");
+                // Console.WriteLine(string.Join("_", strings));
+                // Console.WriteLine(strings.Length);
+            }
+            else
+            {
+                Main();
+            }
+        }
+    }
+}
